85e424311a7bd9e02d171f3646dc71166399af3d
lzzsG <lizizhi122@gmail.com>
2024-04-18 21:00:11 +0800

    to 50

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -7,26 +7,34 @@
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(&vec0);
 
-    println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
+    println!(
+        "{} has length {}, with contents: `{:?}`",
+        "vec0",
+        vec0.len(),
+        vec0
+    );
 
     vec1.push(88);
 
-    println!("{} has length {}, with contents `{:?}`", "vec1", vec1.len(), vec1);
+    println!(
+        "{} has length {}, with contents `{:?}`",
+        "vec1",
+        vec1.len(),
+        vec1
+    );
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    let mut new_vec = vec.clone(); // 克隆传入的向量
 
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
+    new_vec.push(22);
+    new_vec.push(44);
+    new_vec.push(66);
 
-    vec
+    new_vec
 }
